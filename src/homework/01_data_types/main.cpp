c5aa91770db4f57c9dc18dfd6f93c523748ae89b
taylorBacc <157077832+taylorBacc@users.noreply.github.com>
2024-01-26 01:02:38 +0000

    creating multiply_numbers function, testing, and taking user input for use in multiply_numbers

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,27 @@
 ﻿//write include statements
-
+#include "data_types.h"
+#include <iostream>
 
 //write namespace using statement for cout
-
+using std::cout;
+using std::cin;
 
 int main()
 {
-	
+	int num = 0;
+
+	cout<<"Enter a number: ";
+	cin>>num; //user input saved into the variable 'num'
+
+	int result = 0;
+
+	result = multiply_numbers(num);
+
+	cout<<"Value is: "<<result<<"\n";
+
+	int num1 = 4;
+	result = multiply_numbers(num1);
+	cout<<"Next value is: "<<result<<"\n";
 
 	return 0;
 }
