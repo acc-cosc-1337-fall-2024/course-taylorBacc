5f65f15669f0116a1369a4fad1765e7606b2af7e
taylorBacc <157077832+taylorBacc@users.noreply.github.com>
2024-02-09 22:18:07 +0000

    add get_sales_tax_amount and get_tip_amount functions, receive user input and calculate/output full bill

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,11 +1,33 @@
 ﻿//write include statements
+#include<iostream>
+#include "hwexpressions.h"
 
 
 //write namespace using statement for cout
+using std::cout;
+using std::cin;
 
 int main()
 {
-	
+	double meal_amount = 0;
+	double tip_rate = 0;
+	double tip_amount = 0;
+	double tax_amount = 0;
+	double total = 0;
+
+	cout<<"Enter the base cost of the meal: ";
+	cin>>meal_amount;
+
+	tax_amount = get_sales_tax_amount(meal_amount);
+
+	cout<<"Enter the percentage tip amount (ex. 0.20 for 20%): ";
+	cin>>tip_rate;
+
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+	total = meal_amount + tip_amount + tax_amount;
+
+	cout<<"Meal Amount: "<<meal_amount<<"\nSales Tax: "<<tax_amount<<"\nTip Amount: "<<tip_amount<<"\nTotal: "<<total<<"\n";
 
 	return 0;
 }
