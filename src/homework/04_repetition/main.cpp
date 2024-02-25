9e670668eb659f0675c7e0da363b3d0d08cc72c3
taylorBacc <157077832+taylorBacc@users.noreply.github.com>
2024-02-25 04:17:33 +0000

    made factorial and gcd functions, tested factorial and gcd functions, made menu for utilizing created functions

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,8 +1,67 @@
 //write include statements
-
+#include <iostream>
+#include "repetition.h"
 //write using statements
+
+using std::cout, std::cin;
 
 int main() 
 {
+	auto option = 0;
+
+	do{
+		cout<<"1-Factorial\n2-Greatest Common Divisor\n3-Exit\nChoose an option from above(1-3): ";
+		
+		cin>>option;
+
+		if(option == 1)
+		{
+			auto num = 0;
+			cout<<"Choose a number to get the factorial of: ";
+			cin>>num;
+
+			cout<<"The factorial of "<<num<<" is "<<factorial(num)<<".\n";
+		}
+		else if(option == 2)
+		{
+			auto num1 = 0;
+			auto num2 = 0;
+			cout<<"Choose two numbers to get the greatest common divisor of.\nNumber 1: ";
+			cin>>num1;
+			cout<<"Number 2: ";
+			cin>>num2;
+
+			cout<<"The greatest common divisor of "<<num1<<" and "<<num2<<" is "<<gcd(num1, num2)<<".\n";
+		}
+
+		else if(option == 3)
+		{
+			auto exit_option = 'N';
+			cout<<"Are you sure you want to exit(Y/N)?: ";
+
+			do
+			{
+				cin>>exit_option;
+				exit_option = toupper(exit_option);
+				if(exit_option == 'N')
+				{
+					option = 0;
+				}
+				else if(exit_option == 'Y')
+				{
+					cout<<"Exited successfully.\n";					
+				}
+				else
+				{
+					cout<<"Please enter only Y or N: ";
+				}
+			}while(not(exit_option == 'N'));
+		}
+		else
+		{
+			cout<<"\nSelection out of range, select from 1 to 3.\n";
+		}
+	}while(not(option == 3));
+
 	return 0;
 }
--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,33 @@
 //add include statements
-
+#include<repetition.h>
 //add function(s) code here
+int factorial(int num)
+{
+    auto sum = 1;
+    while(num > 0)
+    {
+        sum = sum * num;
+        num--;
+    }
+
+    return sum;
+}
+
+int gcd(int num1, int num2)
+{
+    do
+    {
+        if(num1 < num2)
+        {
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            num1 = num1 - num2;
+        }
+        else if(num1 > num2)
+        {
+            num1 = num1 - num2;
+        }
+    }while(not (num1 - num2 == 0));
+
+    return num1;
+}
