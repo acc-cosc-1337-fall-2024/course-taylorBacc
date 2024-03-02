dfb0703084d9e1fc444088555edccb527a2016fb
taylorBacc <157077832+taylorBacc@users.noreply.github.com>
2024-03-02 14:12:03 +0000

    created get_gc_content, get_dna_complement, and reverse_string functions. Tested newly created functions. created display utilizing created functions.

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,64 @@
 //add include statements
 
+#include "func.h"
+
+using std::string; using std::cout; using std::cin;
+
 //add function code here
+
+double get_gc_content(const string& dna)
+{
+    double gc_amount = 0;
+
+    for(auto ch: dna)
+    {
+        if(ch=='G' || ch=='C')
+        {
+            gc_amount++;
+        }
+    }
+
+    return gc_amount / dna.size();
+}
+
+string get_dna_complement(string dna)
+{
+    string dna_complement;
+
+    for(auto i = 0; i < dna.size(); i++)
+    {
+
+        switch(reverse_string(dna)[i])
+        {
+            case 'A':
+                dna_complement.append(1, 'T');
+                break;
+            case 'T':
+                dna_complement.append(1, 'A');
+                break;
+            case 'C':
+                dna_complement.append(1, 'G');
+                break;
+            case 'G':
+                dna_complement.append(1, 'C');
+                break;
+        }
+    }
+
+    return dna_complement;
+}
+
+string reverse_string(string dna)
+{
+    auto i=1;
+    string dna_reverse;
+
+    for(auto ch: dna)
+    {
+        dna_reverse.append(1, dna[dna.size()-i]);
+
+        i++;
+    }
+
+    return dna_reverse;
+}
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,61 @@
+#include "func.h"
+
+using std::cout; using std::cin;
 
 int main() 
 {
+	auto option = 0;
+
+	do{
+		cout<<"1-Get GC Content\n2-Get DNA Complement\n3-Exit\nChoose an option from above(1-3): ";
+		
+		cin>>option;
+
+		if(option == 1)
+		{
+			std::string sequence;
+			cout<<"Enter a DNA sequence: ";
+			cin>>sequence;
+
+			cout<<"\nThe GC Content of "<<sequence<<" is "<<get_gc_content(sequence)*100<<"%.\n\n";
+		}
+		else if(option == 2)
+		{
+			std::string sequence;
+			cout<<"Enter a DNA sequence: ";
+			cin>>sequence;
+
+			cout<<"\nGenerating reverse complement of the DNA Sequence: "<<sequence<<"\nResult: "<<get_dna_complement(sequence)<<"\n\n";
+		}
+
+		else if(option == 3)
+		{
+			auto exit_option = 'N';
+			cout<<"Are you sure you want to exit(Y/N)?: ";
+
+			do
+			{
+				cin>>exit_option;
+				exit_option = toupper(exit_option);
+				if(exit_option == 'N')
+				{
+					option = 0;
+				}
+				else if(exit_option == 'Y')
+				{
+					cout<<"Exited successfully.\n";					
+				}
+				else
+				{
+					cout<<"Please enter only Y or N: ";
+				}
+			}while(not(exit_option == 'N'));
+		}
+		else
+		{
+			cout<<"\nSelection out of range, select from 1 to 3.\n";
+		}
+	}while(not(option == 3));
 
 	return 0;
 }
